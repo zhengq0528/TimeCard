041f4baccddbd54470c95a1156e47cb4429892b2
Quanita <zhengq0528@gmail.com>
2017-08-14 13:09:35 -0500

    Update db.php

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 //Database, it is a temperate database connection
   $mysql_hostname = 'localhost';
   $mysql_user = 'root';
-  $mysql_password = 'bard$rover';
+  $mysql_password = '*******';
   $mysql_database = "Dickerson Engineer";
   $conn = new mysqli($mysql_hostname, $mysql_user,$mysql_password);
   if ($conn->connect_error) {
